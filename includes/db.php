f607509fcbaec7f794aaf0cc715582e07ae407e5
Revati <revatibelagali@gmail.com>
2025-05-13 12:16:59 +0530

    Initial commit - LeaveManager setup with DB connection

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,6 @@
 <?php
-$con = mysqli_connect("localhost", "leavemanager_user", "shreyas", "leavemanager");
+$con = mysqli_connect("localhost", "leavemanager_user", "Shreyas@123", "leaveman");
+
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
