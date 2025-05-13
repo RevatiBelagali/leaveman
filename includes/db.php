cb3812f5875bafbd0a283804ec1d0d0e700ddb1b
Revati <revatibelagali@gmail.com>
2025-05-13 11:30:11 +0530

    Initial commit - LeaveManager setup with DB connection

--- includes/db.php
+++ includes/db.php
@@ -1,15 +1,6 @@
 <?php
-$servername = "localhost";
-$username = "leavemanager_user"; // the username you created
-$password = "shreyas";           // the password you set for the user
-$dbname = "leavemanager";        // the name of the database
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$con = mysqli_connect("localhost", "leavemanager_user", "shreyas", "leavemanager");
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-echo "Database connected successfully!";
 ?>
